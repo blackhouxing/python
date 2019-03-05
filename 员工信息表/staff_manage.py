a66de193f63710bd8e86e9b1c4338f4fcf4d3443
Administrator <black_xing@163.com>
2019-03-05 19:00:39 +0800

    员工信息解析where语句

--- 员工信息表/staff_manage.py
+++ 员工信息表/staff_manage.py
@@ -20,58 +20,128 @@ def load_db(db_file):
 
 STAFF_DATA = load_db(db_file)
 
+
+
 def print_log(msg,log_type = 'info'):
     if log_type == 'info':
         print(msg)
     elif log_type == 'error':
         print("\033[31;1m%s\033[0m"%msg)
 
-def op_gt():
-    pass
+def op_gt(column,condition_val):
+    """
 
-def op_lt():
-    pass
+    :param column:
+    :param condition_val:
+    :return:
+    """
+    match_records = []
+    for index,val in enumerate(STAFF_DATA[column]):
+        if float(val) > float(condition_val):
+            record = []
+            for col in COLUMN:
+                record.append(STAFF_DATA[col][index])
+            match_records.append(record)
+    return match_records
 
-def op_eq():
-    pass
+def op_lt(column,condition_val):
+    match_records = []
+    for index,val in enumerate(STAFF_DATA[column]):
+        if float(val) < float(condition_val):
+            record = []
+            for col in COLUMN:
+                record.append(STAFF_DATA[col][index])
+            match_records.append(record)
+    return match_records
+def op_eq(column,condition_val):
+    match_records = []
+    for index,val in enumerate(STAFF_DATA[column]):
+        if val == condition_val:
+            record = []
+            for col in COLUMN:
+                record.append(STAFF_DATA[col][index])
+            match_records.append(record)
+    return match_records
 
-def op_like():
-    pass
+def op_like(column,condition_val):
+    match_records = []
+    for index,val in enumerate(STAFF_DATA[column]):
+        if condition_val in val:
+            record = []
+            for col in COLUMN:
+                record.append(STAFF_DATA[col][index])
+            match_records.append(record)
+    return match_records
 
-def syntax_find():
-    pass
+def syntax_find(data_set,query_clause):
+    """
 
-def syntax_update():
-    pass
+    :param data_set:
+    :param query_clause:
+    :return:
+    """
+    filter_cols_temp = query_clause.split("from")[0][4:].split(',')
+    print(filter_cols_temp)
+    filter_cols = [i.strip() for i in filter_cols_temp] #干净的查询条件
+    reformat_data = []
+    for row in data_set:
+        filtered_vals = []
+        for col in filter_cols:
+            col_index = COLUMN.index(col)
+            filtered_vals.append(row[col_index])
+        reformat_data.append(filtered_vals)
+
+    for i in reformat_data:
+        print(i)
+
+def syntax_update(data_set,query_clause):
+    print("这是更新操作")
 
 def syntax_add():
     pass
 
-def syntax_delete():
-
-    pass
+def syntax_delete(data_set,query_clause):
+    print("这是删除操作")
 
 def syntax_where(clause):
     operator = {
-        '>':op_gt(),
-        '<':op_lt(),
-        '=':op_eq(),
-        'like':op_like(),
+        '>':op_gt,
+        '<':op_lt,
+        '=':op_eq,
+        'like':op_like,
         }
     for op_key,op_func in operator.items():
         if op_key in clause:
             column,condition = clause.split(op_key)
-            print(column,condition)
+            match_data = op_func(column.strip(),condition.strip())
 
-            break
-        else:
-            print_log("语法错误：where条件只能支持[>,<,=，like]","error")
+            return match_data
+
+    else:
+        print_log("语法错误：where条件只能支持[>,<,=，like]","error")
 
 def syntax_parser(cmd):
+    """
+
+    :param cmd:
+    :return:
+    """
+    syntax_list = {
+        'find':syntax_find,
+        'del':syntax_delete,
+        'update':syntax_update,
+        'add':syntax_add,
+        }
+
     if cmd.split()[0] in ('find','add','delete','update'):
         query_clause,where_clause = cmd.split('where')
         # print(query_clause,where_clause)
-        syntax_where(where_clause)
+        match_records = syntax_where(where_clause)
+        cmd_action  = query_clause.split()[0]
+        if cmd_action in syntax_list:
+            syntax_list[cmd_action](match_records,query_clause)
+
+
     else:
         print_log("语法错误：[find\\add\\del\\update] [column,,] from [staff_info] where [condition]","error")
 
@@ -82,7 +152,7 @@ def main():
         cmd = input("[staff_data]:").strip()
         # print(cmd)
         if not cmd:continue
-        syntax_parser(cmd)
+        syntax_parser(cmd.strip())
 main()
 
 
